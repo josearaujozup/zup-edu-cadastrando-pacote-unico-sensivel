e37822bf93be43498b470372c836289956ff8db4
Denes <jose.araujo@zup.com.br>
2022-04-27 11:17:44 -0300

    commit inicial

---
+++ src/main/java/br/com/zup/edu/pacoteunico/PacoteunicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.pacoteunico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacoteunicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacoteunicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/pacoteunico/PacoteunicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.pacoteunico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacoteunicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
